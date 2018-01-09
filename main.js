a2f2a3366411784e3a8dc63ab303a6f628950a19
ylfuzzy <ylfuzzy@gmail.com>
2018-01-10 02:07:21 +0800

    Try adding onclick listener to boxs

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app,BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 
 let mainWindow;
 
