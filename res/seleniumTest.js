c3a4bd89b2844e3e3db6064d5c4de5e12750d9ec
ylfuzzy <ylfuzzy@gmail.com>
2018-02-01 00:13:57 +0800

    develop monthly auto route updating process

--- res/seleniumTest.js
+++ res/seleniumTest.js
@@ -18,7 +18,7 @@ let driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 //login(0);
 let asyncLogin = async function() {
-  let waitTime = 5000;
+  let timeout = 5000;
   let trialLogin = 0;
   let homePage = 'https://ecolife.epa.gov.tw/';
   let tf_id = 'cphMain_yAxle_y_login_txtID';
@@ -31,15 +31,15 @@ let asyncLogin = async function() {
   while (trialLogin < 5) {
     try {
       await driver.get(homePage);
-      await driver.wait(until.elementLocated(By.id(tf_id)), waitTime).sendKeys(testID);
-      await driver.wait(until.elementLocated(By.id(tf_passwd)), waitTime).sendKeys(testPasswd);
-      await driver.wait(until.elementLocated(By.id(btn_login)), waitTime).click();
-      //let loginIndicator = await driver.wait(until.elementsLocated(By.id(successfullyLoginID)), waitTime);
+      await driver.wait(until.elementLocated(By.id(tf_id)), timeout).sendKeys(testID);
+      await driver.wait(until.elementLocated(By.id(tf_passwd)), timeout).sendKeys(testPasswd);
+      await driver.wait(until.elementLocated(By.id(btn_login)), timeout).click();
+      //let loginIndicator = await driver.wait(until.elementsLocated(By.id(successfullyLoginID)), timeout);
       let trialToAdmin = 0;
       while (trialToAdmin < 5) {
         try {
-          await driver.wait(until.elementLocated(By.id(btn_goToAdmin)), waitTime).click();
-          await driver.wait(until.titleContains('管理後台'), waitTime);
+          await driver.wait(until.elementLocated(By.id(btn_goToAdmin)), timeout).click();
+          await driver.wait(until.titleContains('管理後台'), timeout);
           return true;
         } catch (err) {
           console.log(err);
@@ -67,31 +67,124 @@ let asyncLogin = async function() {
 }
 
 let asyncUpdateCleanRoute = async function() {
-  let waitTime = 5000;
-  let page_cleanRouteSetting = 'https://ecolifepanel.epa.gov.tw/map/area.aspx';
-  let xpath_btn_firstRow = '//*[@id="cphMain_UPList"]/table/tbody/tr[2]/td[6]/input[1]';
-  let id_btn_edit = 'cphMain_btnEdit';
-  let id_myMap = 'my_map';
-  let class_markers = 'leaflet-marker-pane';
-  try {
-    await driver.get(page_cleanRouteSetting);
-    await driver.wait(until.elementLocated(By.xpath(xpath_btn_firstRow)), waitTime).click();
-    await driver.wait(until.elementLocated(By.id(id_btn_edit)), waitTime).click();
+  let timeout = 5000;
+  let forCleanRouteSetting = false;
+  let page = 'https://ecolifepanel.epa.gov.tw/map/area.aspx';
+  if (!forCleanRouteSetting) {
+    page += '?tab=1';
+  }
+  let trial = 0;
+  let maxTrial = 5;
+  while (trial < maxTrial) {
+    try {
+      // go to setting page
+      console.log('Go to setting page');
+      await goToSettingPageHelper(page);
 
-    // get white squere markers .pngs
-    //let mapContainer = await driver.wait(until.elementLocated(By.id(id_myMap)), waitTime);
-    let squares = await driver.wait(until.elementsLocated(By.css('.leaflet-marker-pane img')));
-    for (let i = 0; i < squares.length; i++) {
-      let img_src = await squares[i].getAttribute('src');
-      if (!img_src.includes('square.png')) {
-        squares.splice(i, 1);
+      // switch user
+      console.log('Try switch user');
+      await switchUserHelper();
+
+      // click white square png
+      console.log('Try click a white square .png');
+      await clickWhiteSquareHelper();
+      break;
+    } catch (err) {
+      console.log('============== final catch ==============');
+      console.log(err);
+      trial++;
+    }
+  }
+}
+
+let goToSettingPageHelper = async function(settingPage) {
+  // wait for elements to be located (milliseconds)
+  let timeout = 5000;
+  let trial = 0;
+  let maxTrial = 5;
+  while (true) {
+    try {
+      // go to setting page
+      let xpath_btn_firstRow = '//*[@id="cphMain_UPList"]/table/tbody/tr[2]/td[6]/input[1]';
+      let id_btn_edit = 'cphMain_btnEdit';
+      await driver.get(settingPage);
+      await driver.wait(until.elementLocated(By.xpath(xpath_btn_firstRow)), timeout).click();
+      await driver.wait(until.elementLocated(By.id(id_btn_edit)), timeout).click();
+      break;
+    } catch (err) {
+      if (trial < maxTrial) {
+        console.log(err);
+        trial++;
+      } else {
+        throw err;
       }
     }
-    for (let i = 0; i < squares.length; i++) {
-      console.log(await squares[i].getAttribute('src'));
+  }
+}
+
+let clickWhiteSquareHelper = async function() {
+  let tiemout = 10000;
+  let trial = 0;
+  let maxTrial = 5;
+  while (true) {
+    try {
+      // get white squere markers .png
+      let css_squareSelector = '.leaflet-marker-pane img';
+      let squares = await driver.wait(until.elementsLocated(By.css(css_squareSelector)), tiemout);
+      for (let i = 0; i < squares.length; i++) {
+        let img_src = await squares[i].getAttribute('src');
+        if (!img_src.includes('square.png')) {
+          squares.splice(i, 1);
+        }
+      }
+      // if the number of squares is even, add a point, else delete the last point.
+      console.log('squares length: ' + squares.length);
+      let actions = driver.actions();
+      if (squares.length % 2 === 0) {
+        await actions.mouseMove(squares[0], {x: 5, y: 0}).click().perform();
+      } else {
+        await squares[squares.length - 1].click();
+      }
+      break;
+    } catch (err) {
+      if (trial < maxTrial) {
+        console.log('click White Square: ' + trial);
+        console.log(err);
+        trial++
+      } else {
+        throw err;
+      }
+    }
+  }
+}
+
+let switchUserHelper = async function() {
+  let timeout = 10000;
+  try {
+    let id_switchUser = 'divShiftList';
+    let xpath_hrefForSwitchUser = '//*[@id="divShiftList"]/a';
+    await driver.wait(until.elementLocated(By.xpath(xpath_hrefForSwitchUser)), timeout).click();
+    
+    // Get user list and switch user
+    let css_userListSelector = 'ul.user_list li';
+    let userList = await driver.wait(until.elementsLocated(By.css(css_userListSelector)), timeout);
+    let activeUserExist = false;
+    for (let i = 0; i < userList.length; i++) {
+      let class_li = await userList[i].getAttribute('class');
+      if (class_li.includes('active')) {
+        await userList[i].findElement(By.css('a')).click();
+        let i_nextUser = (i + 1) % userList.length;
+        await userList[i_nextUser].findElement(By.css('a')).click();
+        activeUserExist = true;
+        break;
+      }
+    }
+    if (!activeUserExist) {
+      await userList[0].findElement(By.css('a')).click();
     }
   } catch (err) {
-    console.log(err);
+    console.log('switch user: ' + trial);
+    throw err;
   }
 }
 
