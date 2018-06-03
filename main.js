3a30134e42279d86519c2c2985231c62c16c2365
unknown <ylfuzzy@gmail.com>
2018-06-03 23:36:04 +0800

    fix: Unexpected end of JSON input error(blank setting.json) in Win7

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const ImagesContainer = require(path.normalize(__base + 'js/utility/imagesContai
 const ImagesProcessor = require(path.normalize(__base + 'js/utility/imagesProcessor'));
 const {app, BrowserWindow, ipcMain, dialog} = electron;
 const {autoUpdater, CancellationToken} = require('electron-updater');
-//const cancellationToken = new CancellationToken();
 autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = false;
 autoUpdater.logger = require('electron-log');
@@ -65,8 +64,6 @@ function saveSetting() {
 
 // Listen for app to be ready
 app.on('ready', function() {
-  /* let chromedriverPath = 'resource' + path.sep + 'chromedriver.exe';
-  console.log('chromedriver path: ', path.join(__dirname, chromedriverPath).replace('app.asar', 'app.asar.unpacked')); */
   // Create new window
   mainWindow = new BrowserWindow({});
 
@@ -122,15 +119,6 @@ ipcMain.on(RENDERER_REQ.INIT_SETTING, function() {
         packet.compressing = settingJson.compressing
         packet.dateChanging = settingJson.dateChanging
         packet.autoUpdating = settingJson.autoUpdating
-        /* if (settingJson.compressing === 'true') {
-          packet.compressing = true;
-        }
-        if (settingJson.dateChanging === 'true') {
-          packet.dateChanging = true;
-        }
-        if (settingJson.autoUpdating === 'true') {
-          packet.autoUpdating = true;
-        } */
       } catch (contentErr) {
         console.log(contentErr);
         packet.compressing = true;
@@ -179,7 +167,6 @@ ipcMain.on(RENDERER_REQ.CHECK_UPDATE.ABORT_DOWNLOAD, function() {
 
 ipcMain.on(RENDERER_REQ.ADD_IMG, function(e, packet) {
   (async function() {
-    //ImagesProcessor.compressImage(packet.imgPath);
     let replyType;
     let imgInfo = await getImageValidity(packet);
     if (imgInfo.isValid) {
@@ -215,11 +202,9 @@ ipcMain.on(RENDERER_REQ.GO.REQUEST_TO_GO, function(e, packet) {
     userInfo.password = packet.password;
     for (tr_n in imagesContainer.tab_inspect) {
       if (imagesContainer.tab_inspect[tr_n].isPaired()) {
-        //await imagesContainer.tab_inspect[tr_n].modify(setting);
         imgSetsForInspect.push(imagesContainer.tab_inspect[tr_n]);
       }
       if (imagesContainer.tab_clean_up[tr_n].isPaired()) {
-        //await imagesContainer.tab_clean_up[tr_n].modify(setting);
         imgSetsForCleanUp.push(imagesContainer.tab_clean_up[tr_n]);
       }
     }
@@ -239,7 +224,6 @@ ipcMain.on(RENDERER_REQ.GO.REQUEST_TO_GO, function(e, packet) {
   }
   console.log('replyType: ' + replyType);
   replyRendererReq(replyType, packet)
-  //autoProcess(packet.id, packet.password);
 });
 
 ipcMain.on(RENDERER_REQ.GO.CONFIRMED, function(e) {
@@ -252,7 +236,6 @@ ipcMain.on(RENDERER_REQ.GO.CONFIRMED, function(e) {
   console.log('for cleanUp: ');
   console.log(imgSetsForCleanUp);
   autoProcess();
-  //testAllUsers();
 });
 
 ipcMain.on(RENDERER_REQ.GO.ABORT, function(e) {
@@ -464,76 +447,6 @@ function getPreloaderFraction(i, total) {
   return i.toString() + '/' + total.toString();
 }
 
-/* async function autoProcess(id, password) {
-  let imgSetsForInspect = [];
-  let imgSetsForCleanUp = [];
-  for (tr_n in imagesContainer.tab_inspect) {
-    if (imagesContainer.tab_inspect[tr_n].isPaired()) {
-      await imagesContainer.tab_inspect[tr_n].modify(setting);
-      imgSetsForInspect.push(imagesContainer.tab_inspect[tr_n]);
-    }
-    if (imagesContainer.tab_clean_up[tr_n].isPaired()) {
-      await imagesContainer.tab_clean_up[tr_n].modify(setting);
-      imgSetsForCleanUp.push(imagesContainer.tab_clean_up[tr_n]);
-    }
-  }
-  for (let i = 0; i < imgSetsForInspect.length; i++) {
-    console.log(imgSetsForInspect[i]);
-  }
-  for (let i = 0; i < imgSetsForCleanUp.length; i++) {
-    console.log(imgSetsForCleanUp[i]);
-  }
-  let isForCleanUp = true;
-  let isForInspect = !isForCleanUp;
-  if (imgSetsForInspect.length !== 0 || imgSetsForCleanUp.length !== 0) {
-    try {
-      let autoModel = new AutoModel();
-      await autoModel.initChromeDriver();
-      await autoModel.login(id, password);
-      for (let i = 0; i < imgSetsForInspect.length; i++) {
-        await autoModel.renewRoute(isForInspect);
-        await autoModel.publishJournal(imgSetsForInspect[i], isForInspect);
-      }
-      for (let i = 0; i < imgSetsForCleanUp.length; i++) {
-        await autoModel.renewRoute(isForCleanUp);
-        await autoModel.publishJournal(imgSetsForCleanUp[i], isForCleanUp);
-      }
-      console.log('============ Finished ============');
-    } catch (err) {
-      console.log('============ MAIN CATCHED! ============');
-      console.log('Discription: ' + err.discription);
-      console.log('Type: ' + err.type);
-      console.log('Origin: ' + err.errOrigin);
-      console.log('OriginalErr: ' + err.originalErr);
-    }
-  } else {
-    console.log('There is no image set can be uploaded');
-  }
-} */
-
-async function testAllUsers() {
-  let ids = ['1002101001', '1002101037', '1002101044', '1002101019', '1002101046', 
-              '1002101041', '1002101054', '1002101040', '1002101049', '1002101033',
-                '1002101029', '1002101006', '1002101035', '1002101013', '1002101027',
-                  '1002101021', '1002101016', '1002101048', '1002101022', '1002101020',
-                    '1002101032', '1002101017', '1002101042', '1002101045', '1002101028',
-                      '1002101050', '1002101047', '1002101039', '1002101023', '1002101055',
-                        '1002101038', '1002101030', '1002101034', '1002101004', '1002101036',
-                          '1002101043', '1002101003', '1002101007', '1002101010', '1002101053',
-                            '1002101018', '1002101025', '1002101051', '1002101052', '1002101015']
-  let isForCleanUp = true;
-  let isForInspect = !isForCleanUp;
-  for (let i = 0; i < ids.length; i++) {
-    //console.log('Now is testing id: ' + ids[i]);
-    let autoModel = new AutoModel();
-    await autoModel.initChromeDriver();
-    await autoModel.login(ids[i], 'east' + ids[i]);
-    await autoModel.renewRoute(isForCleanUp);
-    let newTitle = await autoModel.renewRoute(isForInspect);
-    await autoModel.quitChrome();
-  }
-}
-
 function updateImagesContainer(replyType, packet) {
   switch (replyType) {
     case MAIN_REPLY.ADD_IMG.ACCEPTED:
@@ -543,7 +456,6 @@ function updateImagesContainer(replyType, packet) {
       imagesContainer.deleteImage(packet);
       break;
   }
-//  imagesContainer[packet.tabID][packet.tr_n][packet.imgType] = imgPath;
   console.log(imagesContainer.getImage(packet));
 }
 
